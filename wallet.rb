75cafa85994258806a514972946e507f47b4116b
Scott Key <scottdkey@gmail.com>
2020-02-14 15:38:40 -0700

    wallet class finished?

--- wallet.rb
+++ wallet.rb
@@ -1,2 +1,33 @@
+
+
 class Wallet
+  attr_accessor :wallet
+
+  def initialize
+    @wallet = 0
+  end
+
+  def add_money(ammount)
+    wallet = @wallet.to_i
+    @wallet =  wallet + ammount.to_i
+  end
+
+  def subtract_money(ammount)
+    wallet = @wallet.to_i
+    @wallet = wallet - ammount.to_i
+  end
+
+  def check_wallet
+    puts @wallet.to_i
+  end
+
 end
+
+tester = Wallet.new
+
+
+# wallet = wallet.tester
+tester.add_money(10)
+tester.check_wallet
+tester.subtract_money(5)
+tester.check_wallet
