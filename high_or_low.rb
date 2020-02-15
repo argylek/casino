5683428a7a25a0afb1ba3659401529b91096f806
Scott Key <scottdkey@gmail.com>
2020-02-15 16:41:07 -0700

    high or low class finished

--- high_or_low.rb
+++ high_or_low.rb
@@ -1,34 +1,54 @@
 require_relative 'deck_of_cards'
 require_relative 'wallet'
-# @wallet = Wallet.new
-
 class HighLow
-  attr_accessor :wallet, :wins, :losses
-  def initialize(wallet_value)
-    @wallet = wallet_value
+  attr_accessor :wallet
+  def initialize(wallet_test)
+    @wallet = Wallet.new
+    @wallet.wallet = @wallet.add_money(wallet_test)
     @deck = Deck.new
     @total_bet = 0
     @house_bet = 0
     @user_bet = 0
+    @double_or_nothing = false
+    @play_again = true
+    main_menu
   end
-  def game
+
+  def main_menu
     loop do
       game_menu
-      game_choice = gets.chomp.to_i
-      case game_choice
-      when 1
-        game_start
-        player_bet
-        house_match_bet
-        player_guess
-      when 2
-        @wallet.check_wallet
-      when 3
-        break
-      else
-        puts "That wasn't one of the options"
+      game_menu_choice = gets.chomp.to_i
+      case game_menu_choice
+        when 1
+          @play_again = true
+          until @play_again == false
+          high_low_game
+          end
+          @double_or_nothing = false
+          back_to_wallet
+        when 2
+          puts @wallet.check_wallet
+        when 3
+          exit
+        else
+          puts "That wasn't one of the options"
       end
     end
+  end
+
+  private
+
+  def high_low_game
+      game_start
+      if @double_or_nothing == false
+        player_bet
+        house_match_bet
+      elsif @double_or_nothing == true
+        puts "double or nothing flag is working"
+      else
+        puts "Error - double_or_nothing flag is messed up"
+      end
+      player_guess
   end
 
   def game_menu
@@ -45,27 +65,25 @@ class HighLow
     @down_card = "#{down.rank}#{down.suit}"
     @up_value = up.value
     @down_value = down.value
-    @cards = "The face down card was:#{@up_card}, the face up card was:#{@down_card}."
+    @cards = "The face down card was:#{@down_card}, the face up card was:#{@up_card}."
   end
+
   def game_start
     card_setup
     # puts "down is #{down.rank}#{down.suit}" -- this is here for test purposes to make sure the cards are different
     puts "Two cards are on the table."
-    puts "One card face-down,"
+    puts "One face-up and one face-down"
     puts "and one face-up is the #{@up_card}"
   end
-  private
+
   def house_match_bet
     @house_bet = @user_bet
     @total_bet = @house_bet + @user_bet
     puts "The house has matched your bet of $#{@user_bet}. As is customary."
-    puts "There is $#{@total_bet} on the table."
-    puts "ALL BETS ARE CLOSED"
   end
-  def double_or_nothing
-  end
+
   def player_bet
-    wallet = @wallet
+    wallet = @wallet.wallet
     bet_break = false
     until bet_break == true
       puts "Your wallet currently has $#{wallet}"
@@ -74,17 +92,17 @@ class HighLow
       if @user_bet > wallet
         puts "You don't have that much to bet."
       else
-        @wallet = wallet -= @user_bet
+        @wallet.wallet = wallet -= @user_bet
         puts "You have $#{@user_bet} on the table."
         bet_break = true
       end
     end
   end
 
-  def player_menu
+  def player_guess_menu
     puts "Moment of truth -- "
-    puts "Remember your card is the #{@up_card}"
-    puts "Is your card higher or lower than the face down card?"
+    puts "Remember the face up card is the #{@up_card}"
+    puts "Is the face-down card higher or lower than the face-up card"
     puts "1) Higher"
     puts "2) Lower"
   end
@@ -92,15 +110,15 @@ class HighLow
   def higher_win_conditions
     if @down_value == @up_value
       puts "#{@cards} They were equal, you win!"
-      double_or_nothing
       user_win
     elsif @down_value < @up_value
       puts "#{@cards} You lose!"
       user_lose
     elsif @down_value > @up_value
       puts "#{@cards} You win!"
-      double_or_nothing
       user_win
+    else 
+      puts "higher_win_condidtions error"
     end
   end
 
@@ -114,79 +132,86 @@ class HighLow
     elsif @down_value < @up_value 
       puts "#{@cards} You win!"
       user_win
+    else 
+      puts "lower_win_condidtions error"
     end
   end
 
   def player_guess
+    puts "There is $#{@total_bet} on the table."
+    puts "ALL BETS ARE CLOSED"
     break_condition = false
     until break_condition == true
-    player_menu
-    guess = gets.chomp.to_i
-    if guess == 1
-      @user_guess = true
-      higher_win_conditions
-      double_or_nothing
-      break_condition = true
-      puts @wallet
-    elsif guess == 2
-      @user_guess = false
-      lower_win_conditions
-      break_condition = true
-      puts @wallet
-    else
-      puts "That wasn't a choice, lets try that again."
+      player_guess_menu
+      guess = gets.chomp.to_i
+      if guess == 1
+        @user_guess = true
+        higher_win_conditions
+        break_condition = true
+      elsif guess == 2
+        @user_guess = false
+        lower_win_conditions
+        break_condition = true
+      else
+        puts "That wasn't a choice, lets try that again."
+      end
     end
   end
-end
-def user_win
-  @wallet = @total_bet + @wallet
-end
-def user_lose
-  @wallet = @wallet - @total_bet
-end
-def play_again?
-end
+
+  def back_to_wallet
+    wallet = @wallet.wallet
+    @wallet.wallet = wallet + @total_bet
+    if @total_bet >= 1
+      puts "You've added #{@total_bet} to your wallet!"
+      @wallet.check_wallet
+    elsif @total_bet == 0
+      @wallet.check_wallet
+    else
+      puts "wallet error"
+    end
+  end
+
+  def user_win
+    puts "You currently have won $#{@total_bet}."
+    double_or_nothing
+  end
+
+  def user_lose
+    puts "Oh no! You just lost the pot of! $#{@total_bet}"
+    @total_bet = 0
+    @play_again = false
+  end
+
+  def double_the_bet
+    current_bet = @total_bet
+    @total_bet = current_bet + current_bet
+    puts "The bet has been doubled"
+  end
+
+  def double_or_nothing
+    break_condition = false
+    while break_condition == false
+      puts "Would you like to go double or nothing? Y/N"
+      user_input = gets.chomp.to_s
+      user_conformed_input = user_input.upcase!
+    case user_conformed_input
+      when "Y"
+        double_the_bet
+        @double_or_nothing = true
+        @play_again = true
+        break_condition = true
+      when "N"
+        @play_again = false
+        break_condition = true
+      else
+        puts "that wasn't an option"
+      end
+    end
+  end
 
 end
 
+
+# @wallet = 0
+# puts @wallet
 test = HighLow.new(500)
-test.game
-
-# test.game
-
-# Hi-Lo, or High-Low, is a fairly simple card game. 
-# It uses a standard deck of 52 cards, and it has 
-# players guess whether a certain card is higher or 
-# lower than one showing on the table. There are variations
-# for drinking and gambling, but it's a pretty standard and simple game.
-
-# Number of Players
-# Technically, the game can have any number of players, but at any given
-# time, there are only two players actually playing. The dealer, who is
-# in control of the deck, and the player, who is responsible for guessing
-# the values of cards, are the only participants.
-
-# Dealing and Play
-# After shuffling and cutting the deck, the dealer places one card face-down
-# in front of the player, then another card face-up. The rest of the deck,
-# he sets aside while the player guesses the value.
-
-# Betting
-# After the cards are down, the player places his initial bet. The house
-# matches that bet into the pot. When the player guesses, he wins or
-# loses the pot depending on the outcome of his guess. After that round,
-# the player can pass the bet to another player, or go double or nothing on the next bet.
-
-# Drinking
-# The drinking version of the game has players stacking up drinks in order to pass them
-# to opponents. Each time the player guesses correctly, she takes the card, and a new card
-# is dealt. She can then choose to guess again and get another card, or pass to the next
-# player. If she passes, the next player has to take a drink for each card the first player
-# won. If the original player guesses incorrectly, however, she has to take a drink, plus
-# one for each card she has collected.
-
-# Guessing
-# Based on the card that's currently showing, the player has to guess whether the face-down
-# card is higher or lower than the face-up card. After the guess, the dealer flips the hidden
-# card, and the answer is revealed. If the guess was correct, the player wins and can choose
-# to double or pass; if not, the player loses and the dealer moves on.
