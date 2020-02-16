94f2299ed54c72918193e808281d26c004439a05
Thomas Gaston <thomasgaston11@gmail.com>
2020-02-16 16:41:44 -0700

    not usable stuff

--- casino.rb
+++ casino.rb
@@ -9,7 +9,6 @@ class CasinoMenus
 
   def initialize 
     app_intro_menu
-    
   end  
 
   @slots = Slots.new
@@ -26,40 +25,7 @@ class CasinoMenus
     puts "Welcome to the Casino floor #{@player.name}!"
     game_options
   end
-    
-  def game_options
-      @player.wallet
-      puts "Which game would you like to play?"
-      puts "Slots or High and Low"
-      puts "Press 1 for Slots" 
-      puts "Press 2 for High and Low"
-      puts "Press 3 to reload your wallet"
-      puts "Press 4 to view current wallet amount"
-      puts "Press 5 to exit the app"
-      user_selection
-  end
-  
-  def user_selection
-      game_choice = gets.strip.to_i
-      case game_choice 
-        when 1
-          Slots.new(player)
-        when 2
-          puts "link to High and Low class here"
-        when 3
-          puts "Here's a fresh wallet!"
-          @player.new_wallet
-        when 4
-          puts "You have $#{@player.wallet}.00 remaining"
-        when 5
-          puts "Come gamble again soon!"
-          exit
-        else
-          puts "Invalid choice. Please try again"
-          app_menu
-        end
-      game_options
-    end
+
 end
 
 CasinoMenus.new
--- player.rb
+++ player.rb
@@ -38,32 +38,5 @@ class Player
     @wallet = test.starting_wallet(1000)
     puts "Awesome! You now have $#{@wallet} in your wallet."
   end
-
 end
 # player = Player.new
-
-  def initialize
-    @wallet = 1000
-    puts "******************************************"
-    puts "Hello Player1, Let's get your information set up."
-    puts "******************************************"
-    puts "What is your name?"
-    name = gets.chomp
-    @name = name
-    
-    puts "******************************************"
-    puts "What is your age?"
-    age = gets.strip.to_i
-      if age >= 21
-        puts "You're good to go!"
-      else 
-        puts "Come back when you're older, kid!"
-      end
-    puts "******************************************"
-    
-  end
-
-  def new_wallet
-    @wallet = 1000
-  end
-
