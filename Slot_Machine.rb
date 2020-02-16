f1bfefd4a756189d8b2aace30b1729c05eef01a9
Thomas Gaston <thomasgaston11@gmail.com>
2020-02-15 23:05:16 -0700

    added loop functionality to the app enu and to slot machine

--- Slot_Machine.rb
+++ Slot_Machine.rb
@@ -13,38 +13,40 @@ class Slots
     end
   end
 
+
   def run_slots
-    cash = 500
-    puts "Welcome to the Slot Machine!"
-    puts "Cash total is #{cash}"
-    puts "How much would you like to bet?"
-      @bet = gets.to_i
+    is_running = true
+    while is_running == true
+      cash = 500
+      puts "Welcome to the Slot Machine!"
+      puts "Cash total is #{cash}"
+      puts "How much would you like to bet?"
+        @bet = gets.to_i
 
-      cash -= @bet
+        cash -= @bet
 
-      row1 = ['1', '2', '3', '4'
-      ].sample
+        row1 = ['1', '2', '3', '4'
+        ].sample
 
-      row2 = ['1', '2', '3', '4'
-      ].sample
+        row2 = ['1', '2', '3', '4'
+        ].sample
 
-      row3 = ['1', '2', '3', '4'    
-      ].sample
+        row3 = ['1', '2', '3', '4'    
+        ].sample
 
-      puts "#{row1} #{row2} #{row3}"
+        puts "#{row1} #{row2} #{row3}"
 
-      winnings = @bet * slot_outcomes(row1, row2, row3)
-      
-      puts "You have won #{winnings}"
-      cash += winnings
-      puts "Your cash total is now #{cash}"
+        winnings = @bet * slot_outcomes(row1, row2, row3)
+        
+        puts "You have won #{winnings}"
+        cash += winnings
+        puts "Your cash total is now #{cash}"
 
-      puts "Would you like to play again? Y/N"
-      if gets.strip.upcase == "N"
-        puts "You have ended with #{cash}"
-        exit
-      else
-        run_slots
+        puts "Would you like to play again? Y/N"
+        if gets.strip.upcase == "N"
+          puts "You have ended with #{cash}"
+          is_running = false
+        end
       end
   end
 end
--- casino.rb
+++ casino.rb
@@ -1,5 +1,3 @@
-require_relative 'dice'
-require_relative 'deck_of_cards'
 require_relative 'wallet'
 require_relative 'slot_machine'
 
@@ -9,10 +7,6 @@ class CasinoMenus
   def initialize 
     @wallet = Wallet.new
     @slots = Slots.new
-    @dice = Dice.new
-    @deck = Deck.new
-    @slots = Slots.new
-    app_menu
   end  
   
   
@@ -25,24 +19,31 @@ class CasinoMenus
     puts "Awesome! You now have $#{@wallet.wallet}.00 in your wallet."
   end
     
-  def app_menu 
-    puts "#{@gambler_name} #{@wallet.check_wallet}"
-    puts "Which game would you like to play?"
-    puts "Slots or High and Low"
-    puts "Press 1 for Slots or Press 2 for High and Low"
-    puts "Press 3 to exit the app"
-    @game_choice = gets.to_i
-      case @game_choice 
-        when 1
-          @slots.run_slots
-        when 2
-          # link to High and Low class
-        when 3
-          puts "Come gamble again soon!"
-          exit
-        else
-          puts "Invalid choice. Please try again"
+  def app_menu
+    is_running = true
+    while is_running == true
+      puts "#{@gambler_name} #{@wallet.check_wallet}"
+      puts "Which game would you like to play?"
+      puts "Slots or High and Low"
+      puts "Press 1 for Slots or Press 2 for High and Low"
+      puts "Press 3 to exit the app"
+      @game_choice = gets.to_i
+        case @game_choice 
+          when 1
+            @slots.run_slots
+          when 2
+            # link to High and Low class
+          when 3
+            puts "Come gamble again soon!"
+            is_running = false
+          else
+            puts "Invalid choice. Please try again"
+        end
       end
-      app_menu
     end
 end
+
+app = CasinoMenus.new
+
+app.app_intro_menu
+app.app_menu
