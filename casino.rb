89a2ac71cbaf7b36982585eda246148ca6ec704d
Scott Key <scottdkey@gmail.com>
2020-02-16 22:05:43 -0700

    Updating master for the night

--- casino.rb
+++ casino.rb
@@ -2,30 +2,50 @@ require_relative 'player'
 require_relative 'slot_machine'
 require_relative 'high_or_low'
 require_relative 'player'
+require_relative 'visuals'
 
 
 class CasinoMenus
-  
-
   def initialize 
-    app_intro_menu
-  end  
+    @player = Player.new
+    app
+  end
 
-  @slots = Slots.new
-  @highlow = HighLow.new
-  @player = Player.new
-  
-  
-  def app_intro_menu
-    @player.start
+  def app_menu
+    puts "Which game would you like to play?"
+    puts "1) Slots!"
+    puts "2) High or Low"
+    puts "3) Check Current Wallet Ammount"
+    puts "4) Wallet Reset to $1000"
+    puts "5) Exit the Casino"
   end
     
-  def app_menu
-    @player = Player.new
-    puts "Welcome to the Casino floor #{@player.name}!"
-    game_options
+  def app
+    puts "#{@player.player_name} you have $#{@player.wallet} in your wallet"
+    is_running = true
+    while is_running == true
+      app_menu
+      @game_choice = gets.to_i
+        case @game_choice 
+          when 1
+            @slots = Slots.new(@player)
+            @slots.run_slots
+          when 2
+            highlow = HighLow.new(@player)
+            # highlow.main_menu
+          when 3
+            Wallet.check_wallet
+          when 4
+            @player.new_wallet
+          when 5
+            puts "Come gamble again soon!"
+            is_running = false
+          else
+            puts "Invalid choice. Please try again"
+        end
+    end
   end
-
 end
 
 CasinoMenus.new
+
